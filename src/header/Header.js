d772d7bb045a6da6d4085e73d64f4fa839d747d6
elizadavis <eelizadavis@gmail.com>
2019-07-23 13:58:34 -0400

    Add Links
    Input authorized links

--- src/header/Header.js
+++ src/header/Header.js
@@ -7,6 +7,9 @@ const authenticatedOptions = (
   <React.Fragment>
     <Link to="/change-password">Change Password</Link>
     <Link to="/sign-out">Sign Out</Link>
+    <Link to="/expenses">Expenses</Link>
+    <Link to="/trips">Trips</Link>
+    <Link to="/dashboard">Dashboard</Link>
   </React.Fragment>
 )
 
@@ -25,7 +28,7 @@ const alwaysOptions = (
 
 const Header = ({ user }) => (
   <header className="main-header">
-    <h1>Uber, But For Taxis</h1>
+    <h1>Packing Up Pennies</h1>
     <nav>
       { user && <span>Welcome, {user.email}</span>}
       { user ? authenticatedOptions : unauthenticatedOptions }
